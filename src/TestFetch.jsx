8dacd03154729d099761fcbfd3f7b50a12f43f93
unknown <agussarifudin91@gmail.com>
2025-09-28 23:12:02 +0900

    github

--- src/TestFetch.jsx
+++ src/TestFetch.jsx
@@ -9,7 +9,7 @@ const TestFetch = () => {
   },[]);
 
   const loadUsersData = async ()=>{
-return await axios.get("http://localhost:5000/berita")
+return await axios.get("https://agussarifudin.github.io/json-server-api/db.json")
 .then((response)=>setData(response.data))
 .catch((err)=>console.log(err))
   }
