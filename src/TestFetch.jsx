9a212074030e5901e368298fa3cb719094a25c2e
unknown <agussarifudin91@gmail.com>
2025-09-28 22:38:53 +0900

    nambah consol

--- src/TestFetch.jsx
+++ src/TestFetch.jsx
@@ -13,7 +13,7 @@ return await axios.get("http://localhost:5000/berita")
 .then((response)=>setData(response.data))
 .catch((err)=>console.log(err))
   }
-
+console.log(data)
   return (
     <div>T</div>
   )
